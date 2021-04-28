3fc20216548bc34237f9456860d377f27f45d228
GavrikGal <gavrik.gal@gmail.com>
2021-04-28 22:14:33 +0300

    Первые шаги ФТ для макетирования + стилевого оформления

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -32,6 +32,31 @@ class NewVisitorTest(LiveServerTestCase):
                     raise e
                 time.sleep(0.1)
 
+    def test_layout_and_styling(self):
+        """тест макета и стилевого оформления"""
+        # Эдит открывает домацнюю страницу
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # Она замечает, что поле ввода аккуратно центрировано
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        # Она начинает новый список и видит, что поле ввода там тоже аккуратно центрировано
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
     def test_can_start_a_list_for_one_user(self):
         """тест: можно начать список для одного пользователя"""
 
--- lists/models.py
+++ lists/models.py
@@ -9,4 +9,4 @@ class List(models.Model):
 class Item(models.Model):
     """элемент списка"""
     text = models.TextField(default='')
-    list = models.ForeignKey(List, default=None, on_delete=models.CASCADE)
+    list = models.ForeignKey(List, default=None, on_delete=models.CASCADE, blank=True)
