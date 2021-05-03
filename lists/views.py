4d1ecc7be2ea9e48947551c96eac758dbf61c0a8
GavrikGal <gavrik.gal@gmail.com>
2021-05-03 22:45:35 +0300

    Рефактроризовано представление списка для обработки POST-запросов с новыми элементами

--- lists/views.py
+++ lists/views.py
@@ -4,13 +4,6 @@ from django.shortcuts import render, redirect
 from lists.models import Item, List
 
 
-def add_item(request, list_id):
-    """добавить элемент"""
-    list_ = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect(f'/lists/{list_.id}/')
-
-
 def new_list(request):
     """новый список"""
     list_ = List.objects.create()
@@ -28,6 +21,9 @@ def new_list(request):
 def view_list(request, list_id):
     """представление списка"""
     list_ = List.objects.get(id=list_id)
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'], list=list_)
+        return redirect(f'/lists/{list_.id}/')
     return render(request, 'lists/list.html', {'list': list_})
 
 
