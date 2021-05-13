e3e99670cfbed5a96ad1e9a580ebb7fc565e6d17
GavrikGal <gavrik.gal@gmail.com>
2021-05-14 00:32:10 +0300

    Маркер добавляется к url в письме

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,11 +1,32 @@
 from django.test import TestCase
 from unittest.mock import patch, call
 
+from accounts.models import Token
 import accounts.views
 
 
 class SendLoginEmailViewTest(TestCase):
     """тест представления, которое отправляет сообщение для входа в систему"""
+
+    def test_creates_token_associated_with_email(self):
+        """тест: создается маркер, связанный с электронной почтой"""
+        self.client.post('/accounts/send_login_email', data={
+            'email': 'test.dmitry.gal@gmial.com'
+        })
+        token = Token.objects.first()
+        self.assertEqual(token.email, 'test.dmitry.gal@gmial.com')
+
+    @patch('accounts.views.send_mail')
+    def test_sends_link_to_login_using_token_uid(self, mock_send_mail):
+        """тест: отсылается ссылка на вход в систему, используя uid маркера"""
+        self.client.post('/accounts/send_login_email', data={
+            'email': 'test.dmitry.gal@gmial.com'
+        })
+
+        token = Token.objects.first()
+        expected_url = f'http://testserver/accounts/login?token={token.uid}'
+        (subject, body, from_email, to_list), kwargs = mock_send_mail.call_args
+        self.assertIn(expected_url, body)
 
     @patch('accounts.views.messages')
     def test_adds_success_message_with_mocks(self, mock_messages):
@@ -14,7 +35,7 @@ class SendLoginEmailViewTest(TestCase):
             'email': 'test.dmitry.gal@gmial.com'
         })
 
-        expected = "Проверте совю почту мы отправили вам ссылку, которую можно использовать для входа на сайт."
+        expected = "Проверте свою почту мы отправили вам ссылку, которую можно использовать для входа на сайт."
         self.assertEqual(
             mock_messages.success.call_args,
             call(response.wsgi_request, expected),
@@ -34,7 +55,8 @@ class SendLoginEmailViewTest(TestCase):
 
     def test_redirects_to_home_page(self):
         """тест: переадресуется на домашнюю страницу"""
-        response = self.client.post('/accounts/send_login_email', data={
-            'email': 'test.dmitry.gal@gmail.com'
-        })
+        # response = self.client.post('/accounts/send_login_email', data={
+        #     'email': 'test.dmitry.gal@gmail.com'
+        # })
+        response = self.client.get('/accounts/login?token=abcd123')
         self.assertRedirects(response, '/')
--- accounts/views.py
+++ accounts/views.py
@@ -1,20 +1,33 @@
 from django.core.mail import send_mail
 from django.shortcuts import redirect
 from django.contrib import messages
+from django.urls import reverse
+
+from accounts.models import Token
+
+
+def login(request):
+    """вход в систему"""
+    return redirect('/')
 
 
 def send_login_email(request):
     """отправить сообщение для входа в систему"""
     email = request.POST['email']
+    token = Token.objects.create(email=email)
+    url = request.build_absolute_uri(
+        reverse('login') + '?token=' + str(token.uid)
+    )
+    message_body = f'Use this link to log in:\n\n{url}'
     send_mail(
         'Your login link for Superlists',
-        'body text tbc',
+        message_body,
         'noreply@superlists',
         [email],
     )
 
     messages.success(
         request,
-        "Проверте совю почту мы отправили вам ссылку, которую можно использовать для входа на сайт."
+        "Проверте свою почту мы отправили вам ссылку, которую можно использовать для входа на сайт."
     )
     return redirect('/')
