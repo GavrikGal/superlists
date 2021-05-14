30200b1058b18116e8013757f57b579b677538ef
GavrikGal <Gavrik.gal@gmail.com>
2021-05-14 16:49:47 +0300

    Выхода нет

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.views import auth_logout
 from django.urls import path
 from accounts import views
 
 urlpatterns = [
     path('send_login_email', views.send_login_email, name='send_login_email'),
     path('login', views.login, name='login'),
+    path('logout', auth_logout, {'next_page': '/'}, name='logout'),
 ]
