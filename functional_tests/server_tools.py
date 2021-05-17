b79ff63a5f08c5e00ab1c3198f58078b9e782561
GavrikGal <Gavrik.gal@gmail.com>
2021-05-17 15:39:58 +0300

    Настройка проверки login-logout на промежуточном сервере

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -10,15 +10,13 @@ def _get_manage_dot_py(host):
 def reset_database(host):
     """обнулить базу данных"""
     manage_dot_py = _get_manage_dot_py(host)
-    with settings(host_string=f'ubuntu@{host}'):
-        run('ls')
-        run(f'echo $STAGING_SERVER')
+    with settings(host_string=f'ubuntu@{host}', key_filename='d:\LightsailDefaultKey.pem'):
         run(f'{manage_dot_py} flush --noinput')
 
 
 def create_session_on_server(host, email):
     """создать сеанс на сервере"""
     manage_dot_py = _get_manage_dot_py(host)
-    with settings(host_string=f'ubuntu@{host}'):
+    with settings(host_string=f'ubuntu@{host}', key_filename='d:\LightsailDefaultKey.pem'):
         session_key = run(f'{manage_dot_py} create_session {email}')
         return session_key.strip()
