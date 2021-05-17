ab123f2c8bbe6b0997b01c869ae3dc8c71660732
GavrikGal <gavrik.gal@gmail.com>
2021-05-18 00:20:51 +0300

    Подготовлен ФТ для теста My lists

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -2,6 +2,7 @@ import os
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
 import time
 
@@ -42,6 +43,14 @@ class FunctionalTest(StaticLiveServerTestCase):
     def tearDown(self) -> None:
         """демонтаж"""
         self.browser.quit()
+
+    def add_list_item(self, item_text):
+        """добавить элемент списка"""
+        num_rows = len(self.browser.find_elements_by_css_selector('#id_list_table tr'))
+        self.get_item_input_box().send_keys(item_text)
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        item_number = num_rows + 1
+        self.wait_for_row_in_list_table(f'{item_number}: {item_text}')
 
     def get_item_input_box(self):
         """получить поле ввода для элемента"""
