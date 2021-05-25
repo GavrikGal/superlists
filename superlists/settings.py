bf245a484d5d5fcf299ed566eea293dd75ab892d
GavrikGal <gavrik.gal@gmail.com>
2021-05-26 01:27:19 +0300

    Добавлен исполнитель тестов phantomjs для тестов на javascript. Настройка

--- superlists/settings.py
+++ superlists/settings.py
@@ -85,7 +85,8 @@ WSGI_APPLICATION = 'superlists.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / '../database/db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'NAME': BASE_DIR / '../database/db.sqlite3',
     }
 }
 
